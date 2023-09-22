f36ef62458955adea73c6d727293f304fbb26575
Christopher Smith <smithcl6@vcu.edu>
2023-09-22 16:42:48 -0400

    Product URL now based on environment variable.

--- Cart Service.py
+++ Cart Service.py
@@ -1,6 +1,13 @@
 from flask import Flask, jsonify, request
 import requests
+import os
+from dotenv import load_dotenv
 app = Flask(__name__)
+
+load_dotenv()
+PRODUCTS_URL = os.getenv('PRODUCTS_URL')
+print('GAAAAAAH\n\n')
+print(PRODUCTS_URL)
 
 carts = [
     {"user": 1, "products": [{'name': 'Froot Loops','price': 2.50, 'id': 1, 'quantity': 3}, {'name': 'Milk', 'price': 3.49, 'id': 2, 'quantity': 2}]},
@@ -24,13 +31,13 @@ def check_cart(user_id):
 # Add a specified quantity of a product to a user's cart
 @app.route("/cart/<int:user_id>/add/<int:product_id>", methods=["POST"])
 def retrieve_product(user_id, product_id):
-  product = requests.get(f'http://127.0.0.1:5000/products/{product_id}')  # Check if product exists
+  product = requests.get(f'{PRODUCTS_URL}/products/{product_id}')  # Check if product exists
   if product:
     product_object = {
        "id": product_id,
        "quantity": request.json.get('quantity')
     }
-    retrieved_product = requests.post('http://127.0.0.1:5000/products/retrieve', json=product_object)
+    retrieved_product = requests.post(f'{PRODUCTS_URL}/products/retrieve', json=product_object)
     cart = next((cart for cart in carts if cart['user'] == user_id), None)
     already_in_cart = False  # Indicates if a given product is already in the cart
     for cart_product in cart['products']:
@@ -49,7 +56,7 @@ def retrieve_product(user_id, product_id):
 # Logic here assumes that products that exist in the product service can hold virtually infinite supplies
 @app.route("/cart/<int:user_id>/remove/<int:product_id>", methods=["POST"])
 def return_product(user_id, product_id):
-  product = requests.get(f'http://127.0.0.1:5000/products/{product_id}')  # Check if product exists
+  product = requests.get(f'{PRODUCTS_URL}/products/{product_id}')  # Check if product exists
   if product:
     product_object = {
        "id": product_id,
@@ -72,7 +79,7 @@ def return_product(user_id, product_id):
               product_object['quantity'] = temp
         break
     if valid_product:
-      requests.post('http://127.0.0.1:5000/products/return', json=product_object)
+      requests.post(f'{PRODUCTS_URL}/products/return', json=product_object)
     else:
        return jsonify({'Cart Unchanged': 'User did not have that product'})
     return jsonify({'Cart': cart['products']})
